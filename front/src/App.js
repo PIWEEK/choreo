32cbab7abac6e10caabc0c1aa82fed67e151c4e8
Samuel Gómez <samuel.gomez@kaleidos.net>
2021-07-20 07:53:36 +0200

    Front Home Page

--- front/src/App.js
+++ front/src/App.js
@@ -5,19 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="logo"><span>Choreo</span></h1>
+        <img src="./img/family.jpg" alt="familia abstracta" />
+        <ul className="features">
+          <li>Eliminar los conflictos por las tareas domesticas</li>
+          <li>Visibilizar la carga física y mental</li>
+          <li>Conseguir un reparto de tareas más justo y equilibrado</li>
+        </ul>
       </header>
+      <div className="container">
+        <label>Escribe el código de acceso a tu tablero</label>
+        
+        <div className="code-form">
+          <input type="text"/>
+          <button>Entrar</button>
+        </div>
+        <hr/>
+        <button>Crear un tablero nuevo</button>
+      </div>
     </div>
   );
 }
