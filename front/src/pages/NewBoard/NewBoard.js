7bfc65ba443db4ccc660ae1c6e5537fba6254622
Samuel Gómez <samuel.gomez@kaleidos.net>
2021-07-22 12:07:46 +0200

    FRONT Add Axios and select Avatar

--- front/src/pages/NewBoard/NewBoard.js
+++ front/src/pages/NewBoard/NewBoard.js
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import AvatarSelector from '../../components/AvatarSelector/AvatarSelector'
 import './NewBoard.css';
 
 const NewBoard = () => {
   const [step, setStep] = useState(0);
   const [boardName, setboardName] = useState('');
+  const [showAvatars, setShowAvatars] = useState({visible:false, index:0});
   const [profiles, setProfile] = useState([{avatar:Math.round(Math.random()*10) ,name: ''}])
   const addProfile = ()=> {
     console.log('voy')
     setProfile([...profiles, {avatar:Math.round(Math.random()*10) ,name: ''}])
+  }
+  const openAvatarSelector=(index)=>{
+    setShowAvatars({visible:true, index})
   }
   const handlerEditName = (e, index) => {
     if(profiles.length===1) {
@@ -22,6 +27,22 @@ const NewBoard = () => {
       setProfile([...profiles.slice(0,index), {...profiles[index], name: e.target.value}, ...profiles.slice(index+1)])
     }
   }
+  const selectAvatars = (index) => {
+    console.log(index)
+    if(profiles.length===1) {
+      console.log('Un solo Perfil')
+      setProfile([{...profiles[0], avatar: index}]);
+      setShowAvatars({visible:false, index: profiles.length-1})
+      return;
+    } else if(showAvatars.index === profiles.length-1){
+      console.log('El ultimo Perfil')
+      setProfile([...profiles.slice(0, showAvatars.index), {...profiles[showAvatars.index], avatar: index}])
+    } else {
+      console.log('Cualquier solo Perfil')
+      setProfile([...profiles.slice(0,showAvatars.index), {...profiles[showAvatars.index], avatar: index}, ...profiles.slice(showAvatars.index+1)])
+    }
+    setShowAvatars({visible:false, index: profiles.length-1})
+  }
   return (
     <div className="NewBoard">
       <header className="header">
@@ -32,7 +53,7 @@ const NewBoard = () => {
       {!step &&<div className="grid">
         <label>Escribe el código de acceso a tu tablero</label>
         <input type="text" value={boardName} onChange={(e) => setboardName(e.target.value)} />
-        <button disabled={boardName.length < 3 } onClick={()=>setStep(step+1)}>Guardar</button>
+        <button disabled={boardName.length < 4 } onClick={()=>setStep(step+1)}>Guardar</button>
       </div>}
       {step===1 &&<div className="grid">
         <label>Escribe los nombres de las personas que van a hacer las tareas</label>
@@ -40,15 +61,16 @@ const NewBoard = () => {
           {
             profiles.map((item, index) => (
               <div className="card" key={index}>
-                <div className="avatar"><img src={`./img/avatars/avatar${item.avatar}.png`}></img></div>
+                <div className="avatar" onClick={()=>openAvatarSelector(index)}><img src={`./img/avatars/avatar${item.avatar}.png`}></img></div>
                 <input type="text" value={item.name} onChange={(e)=>handlerEditName(e, index)}/>
               </div>
               )
             )}
           <button  onClick={addProfile}>+</button>
         </div>
-        <button disabled={boardName.length < 3 } onClick={()=>setStep(step+1)}>Guardar</button>
+        <button  disabled={!profiles.length & profiles[0].name.length<3 } onClick={()=>setStep(step+1)}>Guardar</button>
       </div>}
+      {showAvatars.visible && <AvatarSelector handler={selectAvatars} />}
     </div>
   );
 }
